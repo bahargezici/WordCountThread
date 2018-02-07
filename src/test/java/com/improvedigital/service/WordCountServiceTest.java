08f8c7e5751f61c752687c3f03d183d9d9c94788
bahar.gezici <bahar.gezici@veon.com>
2018-02-07 20:00:25 +0100

    tests are added

--- src/test/java/com/improvedigital/service/WordCountServiceTest.java
+++ src/test/java/com/improvedigital/service/WordCountServiceTest.java
@@ -1,28 +1,47 @@
 package com.improvedigital.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * JUnit test for {@link WordCountService}
+ */
+@ContextConfiguration(classes = {WordCountService.class})
+@ComponentScan
+@SpringBootTest(classes = {WordCountService.class})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class WordCountServiceTest {
 
     static String filePath1 = "/Users/bahar.gezici/Desktop/t3.txt";
     static String filePath2 = "/Users/bahar.gezici/Desktop/t4.txt";
 
     static List<String> fileList = new ArrayList<String>();
-    static
-    {
+
+    static {
         fileList.add(filePath1);
         fileList.add(filePath2);
     }
 
-    public static void main(String[] args) {
-        testThreading();
-    }
+//    public static void main(String[] args) {
+//        testThreading();
+//    }
 
 
-    public static void testThreading() {
+    @Before
+    public void testThreading() {
         WordCountService wordCountService = new WordCountService() {
             @Override
             protected void putMap(ConcurrentHashMap<Word, Word> map, File file, String wordInFile) throws InterruptedException {
@@ -39,5 +58,16 @@ public class WordCountServiceTest {
             }
         };
         wordCountService.process(fileList);
+    }
+
+    @Autowired
+    private WordCountService wordCountService;
+
+    /**
+     * Test constructor of {@link WordCountService}
+     */
+    @Test
+    public void testConstructor() {
+        assertNotNull("WordCountService should not be null", new WordCountService());
     }
 }
